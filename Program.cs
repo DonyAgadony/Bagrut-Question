429d647e205da7f5e11cad7a313bb825600942c5
Daniel <donybof1@gmail.com>
2024-09-27 11:30:38 +0300

    Finished Mission

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Numerics;
+
+class Program
+{
+    public static void Main()
+    {
+        int[] ints = [33, 0, 5, 4, 0, 17, 0, 4, 10, 0, 5, 14];
+        Console.WriteLine(BiggestSum(ints));
+    }
+    public static int BiggestSum(int[] ints)
+    {
+        int biggest = int.MinValue;
+        int sum = 0;
+        for (int i = 0; i < ints.Length; i++)
+        {
+            if (ints[i] == 0)
+            {
+                sum = 0;
+                do
+                {
+                    sum += ints[i];
+                    if (i == ints.Length - 1) { return biggest; }
+                    else
+                    {
+                        i++;
+                    }
+
+                    if (sum > biggest) { biggest = sum; }
+                } while (ints[i] != 0);
+                i--;
+            }
+        }
+        return biggest;
+    }
+}
